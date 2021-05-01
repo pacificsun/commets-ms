e67f03b3a5539e79b6018976fdfd00ef683b5335
pacificsuraj <stsurajst@gmail.com>
2021-05-01 23:29:56 +0545

    query service

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 
@@ -17,18 +18,36 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   console.log("content>>", content);
 
   const comments = commentsByPostId[req.params.id] || [];
   console.log("comments>>", comments);
+  // []
   comments.push({ id: commentId, content });
-
+  //comment =>  [{ id: commentId, content}]
   commentsByPostId[req.params.id] = comments;
 
+  // commentsByPostId: {id} = comments
+  // {id: comments}
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => console.log("Listening on port 4001"));
