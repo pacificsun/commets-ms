68964890c825f35bbddf824eff81e8f315f84d29
pacificsuraj <stsurajst@gmail.com>
2021-05-04 09:24:12 +0545

    moderation service added

--- index.js
+++ index.js
@@ -13,10 +13,14 @@ app.use(cors());
 
 const commentsByPostId = {};
 
+// To get comments in a post
+
 app.get("/posts/:id/comments", (req, res) => {
   console.log("comment data", commentsByPostId);
   res.send(commentsByPostId[req.params.id] || []);
 });
+
+// comment creation and send to event bus
 
 app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
@@ -26,7 +30,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   const comments = commentsByPostId[req.params.id] || [];
   console.log("comments>>", comments);
   // []
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
   //comment =>  [{ id: commentId, content}]
   commentsByPostId[req.params.id] = comments;
 
@@ -39,14 +43,42 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
 
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+//  event received from event bus
+
+app.post("/events", async (req, res) => {
   console.log("Received Event", req.body.type);
+
+  const { type, data } = req.body;
+
+  //  send to event bus for moderation
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
   res.send({});
 });
 
